d396a2580449fe091aa9941027e2ae1fd5c20774
M C <monserratdelarosa1b@gmail.com>
2020-08-26 20:31:59 -0500

    le puse el archivo de sillas

---
+++ tienda_sillas.py
@@ -0,0 +1,40 @@
+#Paola De La Rosa A01233794 ITC
+#Ejercicio de tienda de sillas
+# Entrada
+# Una letra mayúscula que representa el tipo de silla 
+# Una letra mayúscula que representa el tipo de cliente
+# Un número entero que indica la cantidad de sillas a comprar.
+# 
+# Salida
+# El subtotal antes de descuento
+# El descuento
+# El total a pagar por el cliente
+
+#Función para obtener el costo de la silla a partir de su tipo
+def costo_silla(tipo_silla):
+    if(tipo_silla == "B"):
+        #costo=700
+        return 700
+    elif(tipo_silla == "E"):
+        #costo=900
+        return 900
+    else:
+        #costo=1500
+        return 1500
+#Función para calcular el descuento con base al tipo de cliente
+def descuento(tipo_cliente, total_compra):
+    if(tipo_cliente == F):
+        descuento=.20
+    elif((tipo_cliente == N) and (20000> total_compra >= 10000))
+    
+    
+def main():
+    #solicitar el tipo de silla
+    #solicitar el tipo de silla
+    #cantidad_sillas
+    #Buscar el costo de la silla en base al tipo
+    #calcular subtotal = cantiad_sillas * costo silla
+    
+
+    
+main()
